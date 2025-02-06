9a9672bd93dd783df66a3b031abc3e9edc9d5ec3
root <root@KG-AMINE>
2025-02-06 17:35:47 +0100

    workflo

--- app/page.tsx
+++ app/page.tsx
@@ -66,7 +66,6 @@ export default function Home() {
           Learn
         </a>
         <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
